0e58268cd15d2ad62268449f526a59fc5c56b7ef
vanha2301 <vanha.cv.vn@gmail.com>
2025-03-22 22:14:39 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || '/react-web', // Sử dụng biến môi trường VITE_BASE_PATH
-});
