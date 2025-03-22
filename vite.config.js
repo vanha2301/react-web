a6009b99087f207a57cb6a17c47e836acee4119a
vanha2301 <vanha.cv.vn@gmail.com>
2025-03-22 22:37:54 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: import.meta.env.VITE_BASE_PATH || '/react-web', // Sử dụng biến môi trường VITE_BASE_PATH
+  build: {
+    outDir: 'dist', // Thư mục đầu ra mặc định là 'dist'
+  },
+});
